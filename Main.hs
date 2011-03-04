ce47ae4d3bde4212df1b2f9a2eb57d3efdd58dfc
payload <payload@lavabit.com>
2011-03-04 17:52:38 +0100

    Return Values

--- Main.hs
+++ Main.hs
@@ -1,19 +1,56 @@
 module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad
+
+data LispVal =
+    Atom String |
+    List [LispVal] |
+    DottedList [LispVal] LispVal |
+    Number Integer |
+    String String |
+    Bool Bool
+    deriving Show
 
 main = do
     args <- getArgs
     putStrLn $ readExpr $ args !! 0
+
+readExpr :: String -> String
+readExpr input =
+    case parse parseExpr "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> show val
+
+parseExpr :: Parser LispVal
+parseExpr =
+    parseAtom <|>
+    parseNumber <|>
+    parseString
+
+parseString :: Parser LispVal
+parseString = do
+    char '"'
+    val <- many $ noneOf "\""
+    char '"'
+    return $ String val
+    
+parseAtom :: Parser LispVal
+parseAtom = do
+    first <- letter <|> symbol
+    rest  <- many $ letter <|> symbol <|> digit
+    let atom = first:rest
+    return $ case atom of
+        "#t" -> Bool True
+        "#f" -> Bool False
+        _    -> Atom atom
+        
+parseNumber :: Parser LispVal
+parseNumber =
+    liftM (Number . read) $ many1 digit
     
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 spaces :: Parser ()
 spaces = skipMany1 space
-
-readExpr :: String -> String
-readExpr input =
-    case parse (spaces >> symbol) "lisp" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found value: " ++ show val
